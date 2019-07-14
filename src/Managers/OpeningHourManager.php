225dd70c5f5fca0e71e50e97705abf7a9a161fb9
railken <railken@protonmail.ch>
2019-07-14 13:05:00 +0200

    phpdoc

--- src/Managers/OpeningHourManager.php
+++ src/Managers/OpeningHourManager.php
@@ -5,6 +5,14 @@ namespace Amethyst\Managers;
 use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
+/**
+ * @method \Amethyst\Models\OpeningHour newEntity()
+ * @method \Amethyst\Schemas\OpeningHourSchema getSchema()
+ * @method \Amethyst\Repositories\OpeningHourRepository getRepository()
+ * @method \Amethyst\Serializers\OpeningHourSerializer getSerializer()
+ * @method \Amethyst\Validators\OpeningHourValidator getValidator()
+ * @method \Amethyst\Authorizers\OpeningHourAuthorizer getAuthorizer()
+ */
 class OpeningHourManager extends Manager
 {
     use ConfigurableManager;
